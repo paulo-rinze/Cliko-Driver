3a126ca105a0cc27c182b9ccfe5c6c6dd178c6db
Paulo Rinze <paulorinze@gmail.com>
2018-07-23 12:58:21 +0200

    First

---
+++ js/config.js
@@ -0,0 +1,8 @@
+/*Driver App Configuration*/
+
+var krms_driver_config ={		
+	'ApiUrl':"https://www.cliko.co.mz/driver/api",	
+	'DialogDefaultTitle':"DriverApp",	
+	'PushProjectID':"AIzaSyBpOMZUfttXATMSMiPh-dQAK9wADrED8i8",	
+	'APIHasKey':"clikoapikey"
+};
